540b253f494f661cbc7b8870909414e09be263db
fedi-nabli <fedinabli@gmail.com>
2024-05-06 14:52:12 +0100

    Created error middleware for auth microservice

---
+++ backend/auth/app.js
@@ -0,0 +1,17 @@
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = 5001
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
