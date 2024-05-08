378f4b6a006151911febac300ebcc59c6729b5cc
fedi-nabli <fedinabli@gmail.com>
2024-05-08 09:37:31 +0100

    Initiated flask app + DatabaseApi Class

---
+++ backend/shop/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+from database.api import DatabaseApi
+
+app = Flask(__name__)
+app.config['secret_key'] = '8a6030d9c5576b19e503878e5925bee4'
+
+db_api = DatabaseApi(app)
+db = db_api.get_db()
+
+if __name__ == '__main__':
+  app.run(debug=True)
