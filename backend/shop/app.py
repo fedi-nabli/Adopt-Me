8eb61c9d552fe7de7302491c7c83e4ce73f435a7
fedi-nabli <fedinabli@gmail.com>
2024-05-09 09:36:35 +0100

    Initiated postresql with shop api

--- backend/shop/app.py
+++ backend/shop/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
 
-from database.api import DatabaseApi
+from database.config import PostgresApi
 
 app = Flask(__name__)
 app.config['secret_key'] = '8a6030d9c5576b19e503878e5925bee4'
 
-db_api = DatabaseApi(app=app)
+db_api = PostgresApi(app=app)
 db = db_api.get_db()
 db_api.create_models()
 
---
+++ backend/shop/database/config.py
@@ -0,0 +1,68 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask
+import psycopg2 as pgsql
+from flask_sqlalchemy import SQLAlchemy
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_NAME = os.getenv('DB_NAME')
+
+class PostgresApi():
+  def __init__(self, app: Flask = None, delete_if_exists: bool = False) -> None:
+    self.app = app
+
+    try:
+      connection = pgsql.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASS
+      )
+
+      connection.autocommit = True
+
+      cursor = connection.cursor()
+
+      sql_query = f"SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('{DB_NAME}'))"
+      cursor.execute(sql_query)
+      check_truth = cursor.fetchall()[0]
+      checked = check_truth[0]
+      self.checked = str(checked)
+
+      if self.checked == "True":
+        # sql_query = f"\c {DB_NAME}"
+        # cursor.execute(sql_query)
+        pass
+      elif self.checked == "False":
+        sql_query = f"CREATE DATABASE {DB_NAME}"
+        cursor.execute(sql_query)
+        # sql_query = f"\c {DB_NAME}"
+        # cursor.execute(sql_query)
+    
+    except(Exception , pgsql.Error) as error:
+      print(f'Error while connecting to postgresql: {error}')
+    finally:
+      if connection:
+        cursor.close()
+        connection.close()
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_NOTIFICATION'] = False
+    self.db = SQLAlchemy(app)
+
+  def get_db(self) -> SQLAlchemy:
+    return self.db
+  
+  def create_models(self) -> None:
+    from models.store_model import StoreItem
+
+    with self.app.app_context():
+      self.db.create_all()
