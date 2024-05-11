8c5ea57a4c05e3d58328ebc109450f9b8c127869
fedi-nabli <fedinabli@gmail.com>
2024-05-11 20:13:22 +0100

    Implemented image upload

--- backend/auth/app.js
+++ backend/auth/app.js
@@ -8,12 +8,24 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/mongob.js'
 import { connectRedis } from './config/redisdb.js'
 
+import uploadRoutes from './routes/uploadRoutes.js'
+
 dotenv.config()
 connectDB()
 connectRedis()
 
 const app = express()
 app.use(express.json())
+
+app.use('/api/upload', uploadRoutes)
+
+const __dirname = path.resolve()
+console.log(path.join(__dirname, '../../uploads'))
+app.use('/uploads', express.static(path.join(__dirname, '../../uploads')))
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
 
 app.use(notFound)
 app.use(errorHandler)
