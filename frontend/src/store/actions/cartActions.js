cdc173f735812809bc8753ce0e57e36e8540bac3
fedi-nabli <fedinabli@gmail.com>
2024-05-19 18:12:58 +0100

    Implemented cart state management

---
+++ frontend/src/store/actions/cartActions.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM
+} from '../constants/cartConstants'
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const {data} = await axios.get(`/shop/${id}`)
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data.id,
+      name: data.name,
+      image: data.image,
+      price: data.price, 
+      qty
+    }
+  })
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import {thunk} from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
 
+import { cartReducer } from './reducers/cartReducers'
 import {
   userLoginReducer,
   userRegisterReducer,
@@ -13,6 +14,7 @@ import {
 } from './reducers/userReducers'
 
 const reducer = combineReducers({
+  cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
